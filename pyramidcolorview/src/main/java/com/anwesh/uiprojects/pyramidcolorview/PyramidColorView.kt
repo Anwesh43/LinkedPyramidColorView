4ee45c165c815e3d5e599eb834de234a0ff20692
Anwesh43 <anweshthecool0@gmail.com>
2020-07-12 18:56:31 +0530

    adding PCNode class

--- pyramidcolorview/src/main/java/com/anwesh/uiprojects/pyramidcolorview/PyramidColorView.kt
+++ pyramidcolorview/src/main/java/com/anwesh/uiprojects/pyramidcolorview/PyramidColorView.kt
@@ -120,4 +120,45 @@ class PyramidColorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PCNode(var i : Int, val state : State = State()) {
+
+        private var next : PCNode? = null
+        private var prev : PCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb  : () -> Unit) : PCNode {
+            var curr : PCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
